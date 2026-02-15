e3b072df4a17feb19e164b5442a58abe3bcf65d6
futsuuu <futsuuu123@gmail.com>
2026-02-15 15:17:40 +0900

    fix(test): don't use `ozone.x.fs`

--- test/helper.lua
+++ test/helper.lua
@@ -1,21 +1,18 @@
-local coro = require("ozone.x.coro")
-local fs = require("ozone.x.fs")
-
 local helper = {}
 
 ---@param entries? table<string, string>
 ---@return string root_dir
 function helper.temp_dir(entries)
-    return coro.wait(function()
-        local root_dir = vim.fs.joinpath(vim.fn.stdpath("cache"), fs.temp_name())
-        assert(fs.create_dir_all(root_dir))
-        for rel_path, content in pairs(entries or {}) do
-            local file_path = vim.fs.joinpath(root_dir, rel_path)
-            assert(fs.create_dir_all(vim.fs.dirname(file_path)))
-            assert(fs.write_file(file_path, content))
-        end
-        return root_dir
-    end)
+    local root_dir = vim.fs.joinpath(vim.fn.stdpath("cache"), vim.fs.basename(os.tmpname()))
+    assert(1 == vim.fn.mkdir(root_dir, "p"))
+    for rel_path, content in pairs(entries or {}) do
+        local file_path = vim.fs.joinpath(root_dir, rel_path)
+        assert(1 == vim.fn.mkdir(vim.fs.dirname(file_path), "p"))
+        local file = assert(io.open(file_path, "w"))
+        assert(file:write(content))
+        assert(file:close())
+    end
+    return root_dir
 end
 
 return helper
