b0b98f6e2abbfdcb6d3075802182684a91f5e4bf
futsuuu <105504350+futsuuu@users.noreply.github.com>
2026-02-24 16:32:03 +0900

    chore: improve example (#17)

--- test/example/default/lua/_build.lua
+++ test/example/default/lua/_build.lua
@@ -4,4 +4,13 @@ ozone.add({
     robot = {
         url = "https://github.com/futsuuu/vim-robot",
     },
+    insx = {
+        url = "https://github.com/hrsh7th/nvim-insx",
+    },
+    fidget = {
+        url = "https://github.com/j-hui/fidget.nvim",
+    },
+    lspconfig = {
+        url = "https://github.com/neovim/nvim-lspconfig",
+    },
 })
